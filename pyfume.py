b12cb7298933eacb906e56b83732c27e7b19fe1b
Fuchs <c.e.m.fuchs@tue.nl>
2020-01-16 16:41:07 +0100

    New pipeline structure

--- pyfume.py
+++ pyfume.py
@@ -1,65 +1,44 @@
-from ModelEstimation import ModelCreator
-from simpfulfier import SimpfulConverter
-from collections import defaultdict
-from math import sqrt
+from Splitter import DataSplitter
+from ModelBuilder import SugenoFISBuilder
+from Clustering import Clusterer
+from EstimateAntecendentSet import AntecedentEstimator
+from EstimateConsequentParameters import ConsequentEstimator
+from Tester import SugenoFISTester
 
-class SugenoFISBuilder(object):
-    """docstring for SugenoFISBuilder"""
-    def __init__(self, path=None, clusters=0, variable_names=None):
-        super(SugenoFISBuilder, self).__init__()
 
-        self._RC = ModelCreator(datapath=path, 
-            nrclus=clusters, 
-            varnames=variable_names
-            )
-
-        self._SC = SimpfulConverter(
-            input_variables_names = variable_names,
-            consequents_matrix = self._RC.cons,
-            fuzzy_sets = self._RC.mfs
-            )
+class BuildTSFIS(object):
+    def __init__(self, datapath, nr_clus, variable_names):
+        self.datapath=datapath
+        self.nr_clus=nr_clus
+        self.variable_names=variable_names
         
-        self._SC.generate_object()
-
-        self.simpfulmodel = self._SC._fuzzyreasoner
-
-
-class SugenoFISTester(object):
-    """docstring for SugenoFISTester"""
-    def __init__(self, model, test_data, golden_standard):
-        super(SugenoFISTester, self).__init__()
-        self._model_to_test = model
-        self._data_to_test = test_data
-        self._golden_standard = golden_standard
-
-    def calculate_error(self, list_of_variables, 
-        list_of_outputs=['OUTPUT']):
-        # read names
-        total_error = defaultdict(float)
+        # Split the data using the hold-out method in a training (default: 75%) 
+        # and test set (default: 25%).
+        ds=DataSplitter(self.datapath)
+        self.x_train, self.y_train, self.x_test, self.y_test = ds.holdout(ds.dataX,ds.dataY,percentage_training=0.75)
         
-        for sample in self._data_to_test:
-            for i, variable in enumerate(list_of_variables):
-                self._model_to_test.set_variable(variable, sample[i])
-            result = self._model_to_test.Sugeno_inference()
-
-            for j, output in enumerate(list_of_outputs):
-                total_error[output] += (result[output] - self._golden_standard[j])**2
-
-        for k,v in total_error.items():
-            total_error[k] = sqrt(v/len(self._data_to_test))
-
-        return total_error
-
-
-class SugenoFISExperimenter(object):
-    """docstring for SugenoFISExperimenter"""
-    def __init__(self, arg):
-        super(SugenoFISExperimenter, self).__init__()
-        self.arg = arg
-                        
-
+        # Cluster the training data (in input-output space) using FCM
+        cl=Clusterer(self.x_train, self.y_train, self.nr_clus)
+        _,self.partition_matrix,_=cl.fcm(cl.data, self.nr_clus)
+        
+        # Estimate the membership funtions of the system (default shape: 2gauss)
+        ae=AntecedentEstimator(self.x_train, self.partition_matrix, mf_shape='2gauss')
+        self.antecedent_sets = ae.determineMF(self.x_train, self.partition_matrix)
+        
+        # Estimate the parameters of the consequent (default: global fitting)
+        ce=ConsequentEstimator(self.x_train, self.y_train, self.partition_matrix)
+        self.consequent_parameters = ce.suglms(self.x_train, self.y_train, self.partition_matrix)
+        
+        # Build a first-order Takagi-Sugeno model using Simpful
+        simpbuilder=SugenoFISBuilder(self.antecedent_sets, self.consequent_parameters, self.variable_names)
+        self.model=simpbuilder.simpfulmodel
+        
+        # Calculate the mean squared error of the model using the test data set
+        test=SugenoFISTester(self.model, self.x_test,self.y_test)
+        error=test.calculate_MSE(variable_names=variable_names)
+        print('The RMSE of the system is', error)
         
 
-if __name__ == '__main__':
-    
-    pass
+if __name__=='__main__':
+ FIS = BuildTSFIS(datapath='../data/dataset6_withlabels.txt', nr_clus=3, variable_names=['v1','v2'])
+ print (dir(FIS.model))
