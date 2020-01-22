f9a028357bedb605112dc132144a9fd8ec98ed5f
Fuchs <c.e.m.fuchs@tue.nl>
2020-01-22 14:43:59 +0100

    add DataLoader

---
+++ LoadData.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class DataLoader(object):
+        def __init__(self, datapath, variable_names=None, normalize=0):
+            if variable_names is None:
+                variable_names = np.genfromtxt(datapath, dtype='str', delimiter=',', max_rows=1)
+                self.output_name=variable_names[-1]
+                self.variable_names=variable_names[:-1]
+                self.data=np.loadtxt(datapath, delimiter=',', skiprows=1)
+            else:
+                self.variable_names=variable_names
+                self.data=np.loadtxt(datapath, delimiter=',')
+                
+            self.dataX=self.data[:,0:-1]
+            self.dataY=self.data[:,-1]
+            
+            if normalize==1:
+                self.dataX = (self.dataX - np.abs(self.dataX).min(axis=0)) / (np.abs(self.dataX).max(axis=0)-np.abs(self.dataX).min(axis=0))
+            
