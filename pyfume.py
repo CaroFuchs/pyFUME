ea4d1e78e333007d9704b59f518924b91db03624
MSN <m.s.nobile@tue.nl>
2020-01-29 15:20:31 +0100

    getter for pyFUME

--- pyfume.py
+++ pyfume.py
@@ -12,8 +12,16 @@ class pyFUME(object):
             self.FIS = BuildTSFIS(self.datapath, self.nr_clus, self.variable_names)
         else:
             raise Exception ("This modeling technique has not yet been implemented.")
-            
+
+    def get_model(self):
+        if self.FIS.FIS.model is None:
+            print ("ERROR: model was not created correctly, aborting.")
+            exit(-1)
+        else:
+            return self.FIS.FIS.model
+
 if __name__=='__main__':
+    
     RMSE=np.ones([20,1])
     for i in range(0,20):
         FIS = pyFUME(datapath='./Tests for WCCI 2020/Concrete_data.csv', nr_clus=4, method='Takagi-Sugeno')
