b3ce0957dd9b349bf3a3654cb49e40117e67c9a9
Fuchs <c.e.m.fuchs@tue.nl>
2021-03-24 16:13:19 +0100

    update conf file

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'scipy.optimize', 'matplotlib', 'matplotlib.pyplot', 'collections', 'math']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+	sys.modules[mod_name] = mock.Mock()
 
 
 # -- Project information -----------------------------------------------------
