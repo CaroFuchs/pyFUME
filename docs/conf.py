5d74352ff8fad58c2491bdc7433af1f0e718f804
Fuchs <c.e.m.fuchs@tue.nl>
2021-03-24 16:02:14 +0100

    Conf file for docs

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,13 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
+
 # -- Project information -----------------------------------------------------
 
 project = 'pyFUME'
