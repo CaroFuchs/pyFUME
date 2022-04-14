a394520d8d8f74e7cba7fd9caf4fb85ab43b4edb
Fuchs <c.e.m.fuchs@tue.nl>
2022-04-14 14:25:31 +0200

    increase version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = 'pyFUME', 
-    version = '0.2.11',
+    version = '0.2.12',
     author = 'Caro Fuchs',
     author_email = 'c.e.m.fuchs@tue.nl',
     description = 'A Python package for fuzzy model estimation',
