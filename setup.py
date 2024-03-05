6e2c39ece2c6dc0d6cd930c7ebd9560ee72324b4
CaroFuchs <59730704+CaroFuchs@users.noreply.github.com>
2024-03-05 18:06:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = 'pyFUME', 
-    version = '0.3.0',
+    version = '0.3.1',
     author = 'Caro Fuchs',
     author_email = 'c.e.m.fuchs@tue.nl',
     description = 'A Python package for fuzzy model estimation',
